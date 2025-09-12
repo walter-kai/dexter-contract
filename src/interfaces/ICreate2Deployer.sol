c55e82d32e3f550fdfe9a26b673c26c764b8eb0f
Walt <122274045+walter-kai@users.noreply.github.com>
2025-09-11 21:47:30 -0700

    remove junk

--- src/interfaces/ICreate2Deployer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface ICreate2Deployer {
-    function deploy(uint256 value, bytes32 salt, bytes memory code) external returns (address);
-    function computeAddress(bytes32 salt, bytes32 codeHash) external view returns (address);
-}
